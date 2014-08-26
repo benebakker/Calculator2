2b3ab1b6a79b67b6a6e49f7bb307365b6081a6a2
Ben Bakker <bbakker@deerfield.edu>
2014-08-26 10:17:49 -0400

    Initial Commit

---
+++ Calculator/CalculatorViewController.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorViewController.h
+//  Calculator
+//
+//  Created by bbakker on 8/26/14.
+//  Copyright (c) 2014 DA CS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorViewController : UIViewController
+
+@end
---
+++ Calculator/CalculatorViewController.m
@@ -0,0 +1,29 @@
+//
+//  CalculatorViewController.m
+//  Calculator
+//
+//  Created by bbakker on 8/26/14.
+//  Copyright (c) 2014 DA CS. All rights reserved.
+//
+
+#import "CalculatorViewController.h"
+
+@interface CalculatorViewController ()
+
+@end
+
+@implementation CalculatorViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
